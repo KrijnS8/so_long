85c5cc99741bab527ddea7bade635c74f55da53b
Krijn Schelvis <kschelvi@f0r4s10.codam.nl>
2023-11-14 13:17:17 +0100

    fixed so_long.h

--- include/so_long.h
+++ include/so_long.h
@@ -6,20 +6,20 @@
 /*   By: kschelvi <kschelvi@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/11/14 13:07:21 by kschelvi      #+#    #+#                 */
-/*   Updated: 2023/11/14 13:11:56 by kschelvi      ########   odam.nl         */
+/*   Updated: 2023/11/14 13:16:57 by kschelvi      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef INCLUDE_H
-# define INCLUDE_H
+#ifndef SO_LONG_H
+# define SO_LONG_H
 
-#include "collectible.h"
-#include "error.h"
-#include "linked_list.h"
-#include "map.h"
-#include "render.h"
-#include "system.h"
-#include "texture.h"
-#include "../Libft/libft.h"
+# include "collectible.h"
+# include "error.h"
+# include "linked_list.h"
+# include "map.h"
+# include "render.h"
+# include "system.h"
+# include "texture.h"
+# include "../Libft/libft.h"
 
 #endif
